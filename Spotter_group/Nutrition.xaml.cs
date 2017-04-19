cdf5e91adf43baeb006798f20dee5c6153f24177
shane <shanemailer@gmail.com>
2017-04-19 12:09:27 -0500

    Added button to nutrition and code for calorie count number colorization.

--- Spotter_group/Nutrition.xaml.cs
+++ Spotter_group/Nutrition.xaml.cs
@@ -25,5 +25,32 @@ namespace Spotter_group
             InitializeComponent();
         }
 
+        private void btnSaveMeal_Click(object sender, RoutedEventArgs e)
+        {
+            
+            string protein = cboBox_Proteins.Text;
+            int proteinCalories = Convert.ToInt32(tboxProteinCalories.Text);
+            int totalCalories = proteinCalories + 0;
+
+            // What is wrong here?
+            lbl_CaloriesToConsume.Text = totalCalories.ToString();
+
+            if (totalCalories < 1500){
+                lbl_CaloriesToConsume.Foreground = Brushes.Green;
+            }
+            else if (totalCalories >= 1500 && totalCalories < 2000)
+            {
+                lbl_CaloriesToConsume.Foreground = Brushes.Yellow;
+            }
+            else if (totalCalories >= 1500 && totalCalories < 2000)
+            {
+                lbl_CaloriesToConsume.Foreground = Brushes.White;
+            }
+            else
+            {
+                lbl_CaloriesToConsume.Foreground = Brushes.Red;
+            }
+
+        }
     }
 }
