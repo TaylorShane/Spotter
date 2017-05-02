3ba6c641c90719a3bf985bd02187a1261396a6dd
drof <drof@DROF-PC>
2017-05-02 09:52:16 -0500

    user global cs

---
+++ Spotter_group/UserGlobal.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spotter_group
+{
+
+     
+
+    public class UserGlobal
+    {
+
+        private string user_name;
+        private string user_password;
+
+        public void set_username(string s)
+        {
+            this.user_name = s;
+        }
+
+        public string get_username
+        {
+            get
+            {
+                return user_name;
+            }
+        }
+
+
+        public string get_user_password
+        {
+            get
+            {
+                return user_password;
+            }
+        }
+
+
+        public void set_user_password(string s)
+        {
+            this.user_password = s;
+        }
+
+
+
+
+    }
+
+
+       
+
+
+    }
+    
+
+
+
+
